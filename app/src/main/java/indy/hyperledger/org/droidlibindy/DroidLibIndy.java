94fc9ca587a68611f5a51cc2581d57b0cc7b415c
Lee byungsun <bslee@coinplug.com>
2020-02-06 14:25:53 +0900

    android version update 27->28

--- app/src/main/java/indy/hyperledger/org/droidlibindy/DroidLibIndy.java
+++ app/src/main/java/indy/hyperledger/org/droidlibindy/DroidLibIndy.java
@@ -1,23 +1,22 @@
 package indy.hyperledger.org.droidlibindy;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.system.ErrnoException;
 import android.system.Os;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
-import com.sun.jna.Library;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
 
 import org.hyperledger.indy.sdk.IndyException;
 import org.hyperledger.indy.sdk.LibIndy;
 import org.hyperledger.indy.sdk.did.Did;
 import org.hyperledger.indy.sdk.did.DidResults;
-import org.hyperledger.indy.sdk.pool.Pool;
 import org.hyperledger.indy.sdk.wallet.Wallet;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -25,7 +24,7 @@ import org.json.JSONObject;
 import java.io.File;
 import java.util.concurrent.ExecutionException;
 
-public class DroidLibIndy extends AppCompatActivity {
+public class DroidLibIndy extends AppCompatActivity{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
